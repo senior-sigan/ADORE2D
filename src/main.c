43ade05b31bd1a4c9b7d8994574ecc63a901d221
Ilya Siganov <ilya.blan4@gmail.com>
2025-09-02 22:02:22 +0600

    deps: add raylib and quickjs

--- src/main.c
+++ src/main.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 #include <raylib.h>
+#include <duktape.h>
 
 int main(void) {
-  printf("Hello world\n");
+  duk_context *ctx = duk_create_heap_default();
+  duk_eval_string(ctx, "1+2");
+  printf("1+2=%d\n", (int) duk_get_int(ctx, -1));
+  duk_destroy_heap(ctx);
 
   InitWindow(800, 600, "ADORE2D");
   while (!WindowShouldClose()) {
